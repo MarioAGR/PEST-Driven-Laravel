365a40bd61c1a0675beb6156508dfd03dbfdf998
Mario Guillen <marioagr95@gmail.com>
2023-02-23 10:14:15 -0600

    TDD see course details part 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageCourseDetailsController;
 use App\Http\Controllers\PageHomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', PageHomeController::class)->name('home');
+Route::get('/', PageHomeController::class)
+    ->name('home');
+
+Route::get('course/{course:slug}', PageCourseDetailsController::class)
+    ->name('course-details');
