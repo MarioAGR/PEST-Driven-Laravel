3ac3a2678878f503360c674b4e0b4fefaa1d1d9d
Mario Guillen <marioagr95@gmail.com>
2023-02-24 21:04:11 -0600

    Push a job for valid Paddle request

--- tests/Feature/WebhookPaddlePurchaseTest.php
+++ tests/Feature/WebhookPaddlePurchaseTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\HandlePaddlePurchaseJob;
 use Spatie\WebhookClient\Models\WebhookCall;
 
 it('stores a paddle purchase request', function () {
@@ -25,7 +26,7 @@ it('stores a paddle purchase request', function () {
         'p_used_price_override' => '1',
         'passthrough' => 'Example passthrough',
         'quantity' => '1',
-        'p_signature' => 'dGVzdA==',
+        'p_signature' => null,
     ]);
 
     // Assert
@@ -41,4 +42,46 @@ it('does not store invalida paddle purchase request', function () {
 
     // Assert
     $this->assertDatabaseCount(WebhookCall::class, 0);
+});
+
+it('dispatches a job for valid paddle request', function () {
+    // Arrange
+    Queue::fake();
+
+    // Act
+    $this->post('webhooks', [
+        'event_time' => '1970-01-01 00:00:01',
+        'p_country' => 'US',
+        'p_coupon' => null,
+        'p_coupon_savings' => null,
+        'p_currency' => 'USD',
+        'p_custom_data' => null,
+        'p_earnings' => '{"vendor_id":"55.5500"}',
+        'p_order_id' => '123456',
+        'p_paddle_fee' => '3.45',
+        'p_price' => '59',
+        'p_product_id' => 'product_id',
+        'p_quantity' => '1',
+        'p_salge_gross' => '59',
+        'p_tax_amount' => '0',
+        'p_used_price_override' => '1',
+        'passthrough' => 'Example passthrough',
+        'quantity' => '1',
+        'p_signature' => null,
+    ]);
+
+    // Assert
+    Queue::assertPushed(HandlePaddlePurchaseJob::class);
+
+});
+
+it('does not dispatch a job for invalid paddle request', function () {
+    // Arrange
+    Queue::fake();
+
+    // Act
+    $this->post('webhooks', []);
+
+    // Assert
+    Queue::assertNothingPushed();
 });
